76136a82db295e1c9bab02df66ed526eac80790f
gabrielvalencia <gabev13@gmail.com>
2022-11-12 06:45:19 -0600

    model property "id" added

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -2,12 +2,17 @@ package org.launchcode.codingevents.models;
 
 public class Event {
 
+    private int id;
+    private static int nextId = 1;
+
     private String name;
     private String description;
 
     public Event(String name, String description) {
         this.name = name;
         this.description = description;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -26,9 +31,27 @@ public class Event {
         this.description = description;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString() {
         return name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
